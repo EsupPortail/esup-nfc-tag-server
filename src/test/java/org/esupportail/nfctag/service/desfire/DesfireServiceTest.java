b8a69bd8023496d477cb5217112efc563a2b2671
Vincent Bonamy <vbonamy@gmail.com>
2025-09-03 15:33:59 +0200

    Fix DesfireServiceTest

--- src/test/java/org/esupportail/nfctag/service/desfire/DesfireServiceTest.java
+++ src/test/java/org/esupportail/nfctag/service/desfire/DesfireServiceTest.java
@@ -28,54 +28,55 @@ import static org.junit.jupiter.api.Assertions.fail;
 @ContextConfiguration(locations={"classpath*:META-INF/spring/applicationContext*.xml"})
 @WebAppConfiguration
 public class DesfireServiceTest {
-	
-	private final Logger log = LoggerFactory.getLogger(getClass());
-	
-	@Autowired(required = false)
-	DesfireTag desfireComueTagEsupSgc;
 
-	@Resource
-	private TagLogDao tagLogDao;
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
-	@Value("${test.desfire.csn:}") 
-	String csnFromConfig; 
-	
-	@Test
-	public void testWithFistCsnFind() throws Exception {
-		Long nbTagLog = tagLogDao.countTagLogs();
-		if(nbTagLog > 0) {
-			List<TagLog> tagLogs = tagLogDao.findTagLogEntries(nbTagLog.intValue() - 1, 1);
-			log.info("test with " + tagLogs.get(0).getCsn());
-			testParseDesfireConfiguration(tagLogs.get(0).getCsn());
-		}
-	}
-	
-	@Test
-	public void testWithConfigCsn() throws Exception {
+    @Autowired(required = false)
+    List<DesfireTag> desfireTags;
+
+    @Resource
+    private TagLogDao tagLogDao;
+
+    @Value("${test.desfire.csn:}")
+    String csnFromConfig;
+
+    @Test
+    public void testWithFistCsnFind() throws Exception {
+        Long nbTagLog = tagLogDao.countTagLogs();
+        if(nbTagLog > 0) {
+            List<TagLog> tagLogs = tagLogDao.findTagLogEntries(nbTagLog.intValue() - 1, 1);
+            log.info("test with " + tagLogs.get(0).getCsn());
+            testParseDesfireConfiguration(tagLogs.get(0).getCsn());
+        }
+    }
+
+    @Test
+    public void testWithConfigCsn() throws Exception {
         assumeTrue(!csnFromConfig.isEmpty(), "no test.desfire.csn configured");
-        assumeTrue(desfireComueTagEsupSgc != null, "no desfireComueTagEsupSgc configured");
-		log.info("test with " + csnFromConfig);
-		testParseDesfireConfiguration(csnFromConfig);
-	}
-	
-	public void testParseDesfireConfiguration(String csn) throws Exception {
-		if(desfireComueTagEsupSgc.getApplications() != null){
-		for(DesfireApplication desApp : desfireComueTagEsupSgc.getApplications()) {
-			for(DesfireFile desFile : desApp.getFiles()){
-				try {
-					desFile.getTagWriteApi().getIdFromCsn(csn);
-				} catch (EsupNfcTagException e) {
-					log.warn("sgc ws error : " + e.getMessage());
-					if(EsupNfcTagErrorMessage.error_esupnfctagexception_serviceunavailable.toString().equals(e.getMessage())) {
-						log.error("sgc ws not responding");
-						fail("error on tagWriteApi for " +  desApp.getDesfireAppId() + " sgc ws not responding");
-					} else {
-						log.warn("sgc ws ok but not card fount with csn = " + csn);
-					}
-				}
-			}
-			
-		}
-		}
-	}
+        assumeTrue(desfireTags != null, "no desfireTags configured");
+        log.info("test with " + csnFromConfig);
+        testParseDesfireConfiguration(csnFromConfig);
+    }
+
+    public void testParseDesfireConfiguration(String csn) throws Exception {
+        for(DesfireTag desfireTag : desfireTags) {
+            if(desfireTag.getApplications() != null){
+                for(DesfireApplication desApp : desfireTag.getApplications()) {
+                    for (DesfireFile desFile : desApp.getFiles()) {
+                        try {
+                            desFile.getTagWriteApi().getIdFromCsn(csn);
+                        } catch (EsupNfcTagException e) {
+                            log.warn("sgc ws error : " + e.getMessage());
+                            if (EsupNfcTagErrorMessage.error_esupnfctagexception_serviceunavailable.toString().equals(e.getMessage())) {
+                                log.error("sgc ws not responding");
+                                fail("error on tagWriteApi for " + desApp.getDesfireAppId() + " sgc ws not responding");
+                            } else {
+                                log.warn("sgc ws ok but not card fount with csn = " + csn);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
